b2c9b81c37cc6d5532a13f7746daf233e8914b81
adamtrann <atra0049@student.monash.edu>
2024-09-06 09:12:32 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "daily_cost_bar_chart.vg.json"; //Define variable which containst he location of the vega-lite json file. 
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    }).catch(console.error);
